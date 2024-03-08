9738dae526b6752d78645b98e9fbc4899771c0e1
nasser25 <nasseremil25@gmail.com>
2024-03-07 21:22:06 -0400

    toastr bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,7 @@
 import { Component } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`,
   styles: []
 })
-export class AppComponent {
-  constructor(private toastr: ToastrService) {
-    this.showSuccess();
-  }
-
-  showSuccess() {
-    this.toastr.success('Hello world!', 'Toastr fun!');
-  }
-}
+export class AppComponent {}
